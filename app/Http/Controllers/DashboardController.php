07aec8edc84fc5ab53c6872d2aabc042103457cc
y2Dev <dub-y@tuta.io>
2022-04-28 12:28:57 -0400

    code aéré

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class DashboardController extends Controller
 {
     //
     public function index(){
+        
+        if (! Gate::allows('access-admin')) {
+            abort(403);
+        }
+
         return view("dashboard") ;
     }
 }
